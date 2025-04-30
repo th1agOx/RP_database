676d8fe42822052ed7278660a9a15f7b734bc227
Thiago <thdarochabp@gmail.com>
2025-04-30 18:10:44 -0300

    config.tabela.conn

---
+++ database/models.py
@@ -0,0 +1,12 @@
+{
+        "email_usuario" : "thiago.pereira.ext@zeiss.com",
+        "email_gestor" : "thiago.pereira.ext@zeiss.com",
+        "titulo" : "Teste",
+        "centro_de_custo" : 650,
+        "dados" : [
+            {
+                "telefone" : "(12)3456-78890",
+                "total" : "30.99"
+            }
+        ]
+}
